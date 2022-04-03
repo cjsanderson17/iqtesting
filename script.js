f80178e8c46d9b5675071be15f61a58ebec5d53e
Callum <cjsanderson17@gmail.com>
2022-04-03 12:50:41 +0100

    added comments setNextQuestion -> SetQuestion given buttons numbers & answer-btns attribute altered hiding of buttons answers added to list questions altered

--- script.js
+++ script.js
@@ -1,120 +1,141 @@
+// defining elements
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const prevButton = document.getElementById('prev-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-btns')
-
 let shuffledQuestions, currentQuestionIndex
 
+
+// defines behaviour for buttons when clicked
 startButton.addEventListener('click', startQuiz)
+
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++
-  setNextQuestion()
+  setQuestion()
 })
+
 prevButton.addEventListener('click', () => {
     currentQuestionIndex--
-    setNextQuestion()
+    setQuestion()
 })
 
+
+// shuffles the questions and unhides them
 function startQuiz() {
   startButton.classList.add('hide')
+  questionContainerElement.classList.remove('hide')
   shuffledQuestions = questions.sort(() => Math.random() - .5)
   currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
+  setQuestion()
 }
 
-function setNextQuestion() {
+
+//
+function setQuestion() {
   resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+
+// applies questions to buttons
 function showQuestion(question) {
   questionElement.innerText = question.question
   question.answers.forEach(answer => {
     const button = document.createElement('button')
     button.innerText = answer.text
-    button.classList.add('btn')
+    button.dataset.number = answer.number
+    button.classList.add('answer-btns', 'btn')
+    // adds correct tag to button iff answer is correct - useful for later
     if (answer.correct) {
-      button.dataset.correct = answer.correct
+        button.dataset.correct = answer.correct
     }
     button.addEventListener('click', selectAnswer)
     answerButtonsElement.appendChild(button)
   })
 }
 
+
+// controls hide for next & prev, deletes previous answer buttons
 function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
-  prevButton.classList.add('hide')
+  nextButton.classList.remove('hide')
+  prevButton.classList.remove('hide')
+  if (currentQuestionIndex + 1 == shuffledQuestions.length) {
+    nextButton.classList.add('hide')
+  } 
+  if (currentQuestionIndex == 0){
+    prevButton.classList.add('hide')
+  }
+
   while (answerButtonsElement.firstChild) {
     answerButtonsElement.removeChild(answerButtonsElement.firstChild)
   }
 }
 
+
+// visual selection: limited to one answer, selection status saved locally
 function selectAnswer(e) {
   const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
   Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
+    removeSelected(button)
   })
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+  selectedButton.classList.add('selected')
+  // gets index of answer if in list already
+  questionIndex = selectedAnswerList.indexOf('q' + (currentQuestionIndex + 1))
+  if (questionIndex != -1) {
+    selectedAnswerList[questionIndex + 1] = selectedButton.dataset.number
   } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
-  } if (currentQuestionIndex > 0){
-    prevButton.classList.remove('hide')
+    selectedAnswerList.push('q' + (currentQuestionIndex + 1), selectedButton.dataset.number)
   }
+  
+  // send to database after 'finish' clicked
 }
 
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
-  } else {
-    element.classList.add('wrong')
-  }
+
+// removes selection from button
+function removeSelected(element) {
+  element.classList.remove('selected')
 }
 
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
-}
 
 const questions = [
   {
     question: 'What is 2 + 2?',
     answers: [
-      { text: '4', correct: true },
-      { text: '22', correct: false }
+      { number: 1, text: '4', correct: true },
+      { number: 2, text: '22', correct: false },
+      { number: 3, text: '222', correct: false },
+      { number: 4, text: '2222', correct: false }
     ]
   },
   {
-    question: 'Who is the best YouTuber?',
+    question: 'Edit',
     answers: [
-      { text: 'Web Dev Simplified', correct: true },
-      { text: 'Traversy Media', correct: true },
-      { text: 'Dev Ed', correct: true },
-      { text: 'Fun Fun Function', correct: true }
+      { number: 1, text: 'Yes', correct: true },
+      { number: 2, text: 'Yeah', correct: true, },
+      { number: 3, text: 'Yep', correct: true, },
+      { number: 4, text: 'Ye', correct: true, }
     ]
   },
   {
-    question: 'Is web development fun?',
+    question: 'Edit',
     answers: [
-      { text: 'Kinda', correct: false },
-      { text: 'YES!!!', correct: true },
-      { text: 'Um no', correct: false },
-      { text: 'IDK', correct: false }
+      { number: 1, text: 'A', correct: false, },
+      { number: 2, text: 'B', correct: true, },
+      { number: 3, text: 'C', correct: false, },
+      { number: 4, text: 'D', correct: false, }
     ]
   },
   {
     question: 'What is 4 * 2?',
     answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
+      { number: 1, text: '6', correct: false, },
+      { number: 2, text: '6', correct: false, },
+      { number: 3, text: '6', correct: false, },
+      { number: 4, text: '8', correct: true, }
     ]
   }
 ]
+
+selectedAnswerList = []
