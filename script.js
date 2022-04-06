67c3766f96116c2c294e696f7b48791194cca7b9
Callum Sanderson <71133457+cjsanderson17@users.noreply.github.com>
2022-04-06 17:48:56 +0100

    merged buttons, back to v.0

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ function resetState() {
 
 // updates progress bar
 function updateProgress() {
-  progressText.innerText = `V.14.Answered: ${selectedAnswerList.length / 4} / ${shuffledQuestions.length}`
+  progressText.innerText = `V.0.Answered: ${selectedAnswerList.length / 4} / ${shuffledQuestions.length}`
   let progressPercentage = ((selectedAnswerList.length / 4) / shuffledQuestions.length) * 100
   progressBarFull.style.width = `${progressPercentage}%`
 }
