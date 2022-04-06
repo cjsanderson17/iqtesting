85d241fb20268bb705a3288afffe19366c29beea
Callum <cjsanderson17@gmail.com>
2022-04-06 22:11:54 +0100

    v6 - reverted changes adjusted landscape height

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ function resetState() {
 
 // updates progress bar
 function updateProgress() {
-  progressText.innerText = `V.5.Answered: ${selectedAnswerList.length / 4} / ${shuffledQuestions.length}`
+  progressText.innerText = `V.6.Answered: ${selectedAnswerList.length / 4} / ${shuffledQuestions.length}`
   let progressPercentage = ((selectedAnswerList.length / 4) / shuffledQuestions.length) * 100
   progressBarFull.style.width = `${progressPercentage}%`
 }
