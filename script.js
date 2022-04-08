e6d8fd4b292f2ae00320719dd6e34bf23d18f18b
Callum <cjsanderson17@gmail.com>
2022-04-08 12:08:19 +0100

    version 9 - IQ rounds to int pushing this version to server

--- script.js
+++ script.js
@@ -229,7 +229,7 @@ function calculateScore(list, noOfQuestions) {
       score++
     }
   }
-  const iq = 140 * score / noOfQuestions
+  const iq = Math.floor(140 * score / noOfQuestions)
   scores = [score, iq]
   return scores
 }
